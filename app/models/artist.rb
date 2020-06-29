27cb800253530a4d926ff51addb807ac9046c144
steve <stevepesce879@gmail.com>
2020-06-28 20:16:48 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,12 @@
+  
 class Artist < ActiveRecord::Base
+  has_many :songs
+
+  def describe
+    self.name + " - " + self.song_count.to_s + " " + "song".pluralize(self.song_count)
+  end
+
+  def song_count
+    self.songs.size
+  end
 end
