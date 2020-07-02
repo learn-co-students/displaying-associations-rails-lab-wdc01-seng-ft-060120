007b34d5e35cdc9fb3e510da1c2d24df3fdb7d63
zato91 <zatokey@gmail.com>
2020-07-01 22:48:57 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,10 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 
   def new
@@ -43,6 +45,7 @@ class ArtistsController < ApplicationController
   end
 
   private
+  
 
   def artist_params
     params.require(:artist).permit(:name)
